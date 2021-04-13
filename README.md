efaf2e7828b4e1b2be98dd8f6bbae6db090c6fc0
Minerva-Gutierrez <201721115@tese.edu.mx>
2021-04-13 13:39:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wwzsva
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wwzsva)
---
+++ src/app/contador/contador.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
+})
+export class ContadorComponent implements OnInit {
+ count: number = 0;
+  constructor() { }
+
+  ngOnInit() {  }
+  aumentar(): void {
+    this.count++;
+  }
+disminuir(): void{
+  this.count--;
+}
+}
